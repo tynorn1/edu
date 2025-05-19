0eabc05019894495ac70a7e0f8b6ed1ca8a1f4c4
Your Name <you@example.com>
2025-05-19 10:03:59 +0200

    new

---
+++ dags/saveasparquet_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import pandas as pd
+
+def excel_to_parquet():
+    df = pd.read_excel("/c/Users/TRAVEEND/OneDrive - Volvo Cars/Data/myproject/excell/source.xlsx")
+    df.to_parquet("/c/Users/TRAVEEND/OneDrive - Volvo Cars/Data/myproject/excell/source.parquet", index=False)
+    print("Excel file converted to 'source.parquet'.")
+
+default_args = {
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='excel_to_parquet_dag',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+    tags=['savetopaarquet'],
+    description='A simple DAG to convert Excel to Parquet',
+) as dag:
+
+    convert_task = PythonOperator(
+        task_id='convert_excel_to_parquet',
+        python_callable=excel_to_parquet,
+    )
